d975f4ea6e0693a37c30f7c1bc9a7c623f3bcafe
weenie89 <weenie89@192.168.0.101>
2024-06-06 19:48:19 +0800

    feat: 新增 winston 日志记录功能

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const readEnv = require('./src/config')
 const authMiddleware = require('./src/middlewares/auth')
 const adminMiddleware = require('./src/middlewares/admin')
+const winston = require('winston')
 
 const {
   movies,
@@ -44,6 +45,57 @@ function connectDB() {
       // process.exit(1)
     })
 }
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    //
+    // - Write all logs with importance level of `error` or less to `error.log`
+    // - Write all logs with importance level of `info` or less to `combined.log`
+    //
+    new winston.transports.File({ filename: 'log/error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'log/combined.log' }),
+    new winston.transports.Http({
+      level: 'info',
+      filename: 'log/http.log'
+    })
+  ],
+  exceptionHandlers: [
+    new winston.transports.File({ filename: 'log/exceptions.log' })
+  ],
+  rejectionHandlers: [
+    new winston.transports.File({ filename: 'log/rejections.log' })
+  ]
+})
+logger.http(
+  'http-error',
+  new winston.transports.Http({
+    level: 'info',
+    filename: 'log/http.log'
+  })
+)
+
+// Promise.reject('aaa').then()
+// throw new Error('aaa')
+
+// new winston.ExceptionHandler(logger)
+// new winston.RejectionHandler((err) => logger(err))
+
+//
+// If we're not in production then log to the `console` with the format:
+// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
+//
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  )
+}
+
+// process.on('unhandledRejection', (err) => logger.error(err))
 
 function useMiddleware() {
   app.use(morgan('dev'))
@@ -97,3 +149,5 @@ function run() {
 useMiddleware()
 connectDB()
 run()
+
+// Promise.reject('something wrong').then()
